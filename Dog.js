1d6a13ca3e3ba948f9fe96ca622f4cad8525ffee
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2023-03-10 13:29:10 -0500

    completed Tindog Project

--- Dog.js
+++ Dog.js
@@ -1,29 +1,15 @@
 
-
-
   class Dogs {
     constructor(data) {
       Object.assign(this, data)
     }
 
     displayData() {
-      const {name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this
+      const {name, avatar, age, bio} = this
       return `
-          <div class="img-container">
-            <img class="dog-img" src=${avatar} alt="teddy">
+            <img class="dog-img" src=${avatar} alt="${name}">
             <h2 class="name">${name}, ${age}</h2>
             <p class="greeting">${bio}</p>
-            <img class="like-status" src="./images/badge-like.png" alt="like-img">
-            <img class="nope-status" src="./images/badge-nope.png" alt="like-img">
-         </div>
-         <div class="btn-container">
-                  <button class="nope-btn">
-                      <img src="./images/icon-cross.png" alt="nope-btn">
-                  </button>
-                  <button class="like-btn">
-                      <img src="./images/icon-heart.png" alt="like-btn">
-                  </button>
-              </div>
          `
     }
   } 
--- index.js
+++ index.js
@@ -1,18 +1,77 @@
 import dogs from "./data.js"
 import Dogs from "./Dog.js"
 
+const imgContainer = document.querySelector(".img-container")
+const nopeBtn = document.querySelector(".nope-btn")
+const likeBtn = document.querySelector(".like-btn")
+const likeStatus = document.querySelector(".like-status")
+const nopeStatus = document.querySelector(".nope-status")
 
-const imgSection = document.getElementById("img-section")
 
+let arrNum = [0, 1, 2]
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+//Get next Dog Func
+function getNextDog() {
+  let nextNumber = arrNum.shift()
+  let getNextDog = new Dogs(dogs[nextNumber])
+  return getNextDog ? getNextDog : {}
 }
+// 
 
+// Display data
+let getDogs = getNextDog()
 function render() {
-  imgSection.innerHTML += randomDogs.displayData()
+  imgContainer.innerHTML = getDogs.displayData()
+}
+render()
+// 
+
+// Click Status
+
+// Like
+function gotLiked() {
+  getDogs.hasBeenLiked = true
+  getDogs.hasBeenSwiped = true
+  likeStatus.classList.add("status-active")
+  nopeStatus.classList.remove("status-active")
+  if (getDogs.hasBeenLiked && getDogs.hasBeenSwiped) {
+    if (arrNum.length > 0) {
+      setTimeout(() => {
+        getDogs = getNextDog()
+        render()
+        likeStatus.classList.remove("status-active")
+      }, 2000)
+    } else {
+      setTimeout(() => {
+        imgContainer.classList.add("display-none")
+      }, 3000)
+    }
+  }
 }
 
-const randomDogs = new Dogs(dogs[getRandomInt(dogs.length)])
-render()
+// Nope
+function gotNope() {
+  getDogs.hasBeenSwiped = true
+  nopeStatus.classList.add("status-active")
+  likeStatus.classList.remove("status-active")
+
+  if (getDogs.hasBeenSwiped) {
+    if (arrNum.length > 0) {
+      setTimeout(() => {
+        getDogs = getNextDog()
+        render()
+        nopeStatus.classList.remove("status-active")
+      }, 2000)
+    } else {
+      setTimeout(() => {
+        imgContainer.classList.add("display-none")
+      }, 2000)
+    }
+    
+  }
+}
+
+likeBtn.addEventListener("click", gotLiked)
+nopeBtn.addEventListener("click", gotNope)
+
 
